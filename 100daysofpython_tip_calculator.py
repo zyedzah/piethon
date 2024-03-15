c536da31220e180aa3e8c0a47c2d56ce9b31fb92
zyedzah <sudosyed@ccpbot>
2024-03-14 21:59:21 -0400

    not ready yet

---
+++ 100daysofpython_tip_calculator.py
@@ -0,0 +1,8 @@
+print("WELCOME TO THE TIP CALCULATOR!!!!!!!!!!!!!")
+bill = float(input("How much was your bill?: $"))
+tip = int(input("Enter the percent you would like to tip: ")
+split = float(input("Split between how many people?: "))
+bill_result = bill / split
+print("You should tip " + bill_result + ".")
+
+
