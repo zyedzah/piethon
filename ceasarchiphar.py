4c9011f03b304aafa50c03ce43b8e4fd29c6a128
Your Name <sudosyed@DESKTOP-LD76GOQ>
2024-03-28 20:38:32 -0400

    still work in progress now vigenere cipher

--- ceasarchiphar.py
+++ ceasarchiphar.py
@@ -1,9 +1,27 @@
-text = 'Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+text = 'Hello Zaira'
+custom_key = 'python'
 
-for char in text.lower():
-    index = aplhabet.find(char)
-    new_index = index + shift
-    new_char = alphabet[new_index]
-    print('char:', char, 'new char', new_char)
+def vigenere(message, key, direction):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+    
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    
+    return encrypted_text
+encryption = vigenere(text, custom_key, 1)
+print(encryption)
